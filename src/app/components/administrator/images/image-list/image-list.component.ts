a913925c95e61dc2dc71cb606303c670fcd33f5d
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-08-07 17:07:44 -0400

    Se usa servidor para insertar imagenes en html determinado para galeria de la pagina web

--- src/app/components/administrator/images/image-list/image-list.component.ts
+++ src/app/components/administrator/images/image-list/image-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ImageService } from 'src/app/shared/image.service';
 
 @Component({
   selector: 'app-image-list',
@@ -7,13 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class ImageListComponent implements OnInit {
+  imageList: any[];
+  rowIndexArray: any[];
 
-
-  constructor() { }
+  constructor(private service: ImageService) { }
 
   ngOnInit() {
-   
-   
+    this.service.getImageDetailList();
+    this.service.imageDetailList.snapshotChanges().subscribe(
+      list => {
+       
+        this.imageList = list.map(item => { return item.payload.val(); });
+        this.rowIndexArray =  Array.from(Array(Math.ceil((this.imageList.length+1) / 3)).keys());
+      }
+    );
   }
 
 }
