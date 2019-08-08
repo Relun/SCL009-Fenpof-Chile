3f88fe9a9ad97579026dec6c2d9c232e7d01e781
Patriciavaldebenito <patricia.valdebenito@usach.cl>
2019-08-08 10:35:40 -0400

    Se valida ingreso de administrador a publicacion de eventos

--- src/app/components/administrator/post-user/post-user.component.ts
+++ src/app/components/administrator/post-user/post-user.component.ts
@@ -2,27 +2,51 @@ import { Component, OnInit } from '@angular/core';
 import { Postuser } from 'src/app/post-user';
 import { PostUserService } from 'src/app/services/post-user.service';
 
+import { ImageService } from 'src/app/shared/image.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+
+
+
+
 @Component({
   selector: 'app-post-user',
   templateUrl: './post-user.component.html',
   styleUrls: ['./post-user.component.css']
 })
 export class PostUserComponent implements OnInit {
-
+  public isLogged: boolean = false;
   postusers:Postuser[];
 
   createPost:Postuser; // se declara para poder tomar el string que trae el html
 
-  constructor( public postUserService: PostUserService ) { }
+  constructor( public postUserService: PostUserService,private service: ImageService, private authService: AuthService, private router: Router, private afsAuth: AngularFireAuth ) { }
 
   ngOnInit() {
+    this.getCurrentUser();
     this.postUserService.getRead().subscribe(postuser =>{
       this.postusers = postuser; 
     });
 
   }
 
+  getCurrentUser(){
 
+    this.authService.isAuth().subscribe(auth => {
+
+      if(auth){
+        console.log('user logged');
+        this.isLogged = true;
+
+      } else {
+        console.log('user NOT logged');
+        this.isLogged = false;
+      }
+
+    });
+  } 
  /*Funcion para crear post y que se guarde en firestore */
   sendPost( mypost : string): void  {
     console.log("Llego hasta sendPost");
